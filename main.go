c5545abe86a3e8d93cc5cfc84ee7f348c32c3677
Alexis Salgado Tacuri <xel.salg@gmail.com>
2019-06-06 19:31:47 -0500

    templates

--- main.go
+++ main.go
@@ -6,23 +6,60 @@ import (
 	"net/http"
 )
 
-//var tpls = template.Must(template.New("T").ParseFiles(
-//	"templates/index.html",
-//	"templates/footer.html",
-//	"templates/header.html",
-//	"templates/registro.html",
-//))
+type User struct {
+	Username string
+}
 
-var tpls = template.Must(template.New("T").ParseGlob("templates/*.html"))
+type Comment struct {
+	Title       string
+	Content     string
+	ContentHtml template.HTML
+}
+
+var tpls = template.Must(template.New("T").ParseGlob("templates/**/*.html"))
+
+var errTpl = template.Must(template.ParseFiles("templates/error.html"))
+
+func handlerError(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	_ = errTpl.Execute(w, err.Error())
+}
+
+func renderTpl(w http.ResponseWriter, name string, data interface{}) {
+	w.Header().Set("Content-Type", "text/html")
+	err := tpls.ExecuteTemplate(w, name, data)
+	if err != nil {
+		handlerError(w, err)
+	}
+}
+
+func comment(w http.ResponseWriter, r *http.Request) {
+
+	contentHtml := template.HTML("<h1>contenido</h1>")
+	c := Comment{"titulo",
+		"<h1>contenido</h1>",
+		contentHtml,
+	}
+	renderTpl(w, "comments/comment", c)
+}
 
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := tpls.ExecuteTemplate(w, "registro", nil)
-		if err != nil {
-			panic(err)
-		}
+		renderTpl(w, "index", nil)
 	})
+
+	user := User{"alexis"}
+	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		renderTpl(w, "user", user)
+	})
+
+	http.HandleFunc("/comment", comment)
+	//c := Comment{"titulo", "contenido"}
+	//http.HandleFunc("/comment", func(w http.ResponseWriter, r *http.Request) {
+	//	renderTpl(w, "comments/comment", c)
+	//})
+
 	log.Println("el servidor escucha:3000")
 	log.Fatal(http.ListenAndServe("localhost:3000", nil))
 }
